0b94c8e6a516f474e181e81fee4c93272063bb7e
marco <you@example.com>
2022-11-14 01:32:12 -0500

    google authentication config

--- src/helpers/googleVerify.js
+++ src/helpers/googleVerify.js
@@ -6,11 +6,9 @@ const googleVerify = async (idToken) => {
     const client = new OAuth2Client("954172892896-8fnhbgil5quoo3bag1bk04s9vheaga7s.apps.googleusercontent.com")
     const ticket = await client.verifyIdToken({
         idToken,
-        audience: "954172892896-8fnhbgil5quoo3bag1bk04s9vheaga7s.apps.googleusercontent.com"
+        requiredAudience: "954172892896-8fnhbgil5quoo3bag1bk04s9vheaga7s.apps.googleusercontent.com",
     })
-
     const payload = ticket.getPayload()
-
     return {
         email: payload.email,
         name: payload.given_name,
